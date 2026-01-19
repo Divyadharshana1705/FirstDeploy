7949eee2cf365d5959b33afc49281bef5538317e
Divya Dharshana <divyadharshana1705@gmail.com>
2026-01-19 18:27:28 +0530

    Initial commit: Lung histopathology detection project

---
+++ model.py
@@ -0,0 +1,32 @@
+from tensorflow import keras
+
+from tensorflow.keras import layers
+import os
+
+
+def createModel(train_data=None):
+    if os.path.exists('./model/lung.h5') and train_data is None:
+        try:
+            print(_name_)
+            model = keras.models.load_model('./model/lung.h5')
+            print("returned")
+            return model
+        except Exception as e:
+            print("error")
+
+
+    elif train_data is not None:
+        model = keras.Sequential([
+
+            keras.Input(shape=train_data.shape[1:]),
+            layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+            layers.MaxPooling2D(pool_size=(2, 2)),
+            layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+            layers.MaxPooling2D(pool_size=(2, 2)),
+            layers.Flatten(),
+            layers.Dense(128, activation="relu"),
+
+            layers.Dropout(0.5),
+            layers.Dense(3, activation="softmax")
+        ])
+        return model
